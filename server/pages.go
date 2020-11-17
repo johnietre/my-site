5760f04eba62f308a7c397ab35cb494b53857553
johnietre <johnietrebus@gmail.com>
2020-11-17 12:20:31 -0600

    Added login/register

--- server/pages.go
+++ server/pages.go
@@ -8,9 +8,9 @@ import (
 	"strings"
 )
 
-/*
- Handle the GET requests to get the specific convo
-*/
+type PageData struct {
+	ErrMsg string
+}
 
 var (
 	pageLogger      *log.Logger
@@ -18,73 +18,42 @@ var (
 	stocksTemplates *template.Template
 	chatTemplates   *template.Template
 	convoTemplates  *template.Template
-	baseTempName    string
+	loginTemplates *template.Template
+	registerTemplates *template.Template
+	tDir string
 )
 
 func init() {
 	pageLogger = log.New(os.Stdout, "Page Server: ", log.LstdFlags)
 	// Conditional possibly unneeded
 	if strings.Contains(cwd, "server") {
-		baseTempName = "../templates/base.html"
-		homeTemplates = template.Must(template.ParseFiles(
-			baseTempName,
-			"../templates/home.html"))
-		stocksTemplates = template.Must(template.ParseFiles(
-			baseTempName,
-			"../templates/stocks.html"))
-		chatTemplates = template.Must(template.ParseFiles(
-			baseTempName,
-			"../templates/chat.html"))
-		convoTemplates = template.Must(template.ParseFiles(
-			baseTempName,
-			"../templates/chat.html"))
+		tDir = "../templates/"
 	} else {
-		baseTempName = "templates/base.html"
-		homeTemplates = template.Must(template.ParseFiles(
-			baseTempName,
-			"templates/home.html"))
-		stocksTemplates = template.Must(template.ParseFiles(
-			baseTempName,
-			"templates/stocks.html"))
-		chatTemplates = template.Must(template.ParseFiles(
-			baseTempName,
-			"templates/chat.html"))
-		convoTemplates = template.Must(template.ParseFiles(
-			baseTempName,
-			"templates/chat.html"))
+		tDir = "./templates/"
 	}
+	// Load the templates
+	parse()
 }
 
 func parse() {
-	if strings.Contains(cwd, "server") {
-		baseTempName = "../templates/base.html"
-		homeTemplates = template.Must(template.ParseFiles(
-			baseTempName,
-			"../templates/home.html"))
-		stocksTemplates = template.Must(template.ParseFiles(
-			baseTempName,
-			"../templates/stocks.html"))
-		chatTemplates = template.Must(template.ParseFiles(
-			baseTempName,
-			"../templates/chat.html"))
-		convoTemplates = template.Must(template.ParseFiles(
-			baseTempName,
-			"../templates/convo.html"))
-	} else {
-		baseTempName = "templates/base.html"
-		homeTemplates = template.Must(template.ParseFiles(
-			baseTempName,
-			"templates/home.html"))
-		stocksTemplates = template.Must(template.ParseFiles(
-			baseTempName,
-			"templates/stocks.html"))
-		chatTemplates = template.Must(template.ParseFiles(
-			baseTempName,
-			"templates/chat.html"))
-		convoTemplates = template.Must(template.ParseFiles(
-			baseTempName,
-			"templates/convo.html"))
-	}
+	homeTemplates = template.Must(template.ParseFiles(
+		tDir+"base.html",
+		tDir+"home.html"))
+	stocksTemplates = template.Must(template.ParseFiles(
+		tDir+"base.html",
+		tDir+"stocks.html"))
+	chatTemplates = template.Must(template.ParseFiles(
+		tDir+"base.html",
+		tDir+"chat.html"))
+	convoTemplates = template.Must(template.ParseFiles(
+		tDir+"base.html",
+		tDir+"convo.html"))
+	loginTemplates = template.Must(template.ParseFiles(
+		tDir+"base.html",
+		tDir+"login.html"))
+	registerTemplates = template.Must(template.ParseFiles(
+		tDir+"base.html",
+		tDir+"register.html"))
 }
 
 func homePageHandler(w http.ResponseWriter, r *http.Request) {
@@ -116,4 +85,60 @@ func chatPageHandler(w http.ResponseWriter, r *http.Request) {
 			pageLogger.Println(err)
 		}
 	}
+}
+
+func loginPageHandler(w http.ResponseWriter, r *http.Request) {
+	parse()
+	pageData := newPageData()
+	loggedIn := false
+	if r.Method == http.MethodPost {
+		email := r.FormValue("email")
+		password := r.FormValue("password")
+		println(email, password)
+		if email != "johnietre@gmail.com" {
+			pageData.ErrMsg = "Invalid email or password"
+		} else {
+			loggedIn = true
+		}
+	}
+	if loggedIn {
+		homePageHandler(w, r)
+	} else {
+		if err := loginTemplates.ExecuteTemplate(w, "login.html", pageData); err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			pageLogger.Println(err)
+		}
+	}
+}
+
+func registerPageHandler(w http.ResponseWriter, r *http.Request) {
+	parse()
+	pageData := newPageData()
+	registered := false
+	if r.Method == http.MethodPost {
+		fname := r.FormValue("fname")
+		lname := r.FormValue("lname")
+		email := r.FormValue("email")
+		password := r.FormValue("password")
+		println(fname, lname, email, password)
+		if email == "johnietre@gmail.com" {
+			pageData.ErrMsg = "Email already assigned to account"
+		} else {
+			registered = true
+		}
+	}
+	if registered {
+		http.Redirect(w, r, "/", http.StatusAccepted)
+	} else {
+		if err := registerTemplates.ExecuteTemplate(w, "register.html", pageData); err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			pageLogger.Println(err)
+		}
+	}
+}
+
+// Delete???
+func newPageData() *PageData {
+	page := &PageData{""}
+	return page
 }
