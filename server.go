1ffb6f438b3dc6da7012cfd810b3891e30e284d3
johnietre <johnietrebus@gmail.com>
2021-01-24 16:31:05 -0600

    Option to set ip/port environ vars

--- server.go
+++ server.go
@@ -3,14 +3,23 @@ package main
 import (
 	"log"
 	"net/http"
+  "os"
 )
 
 func main() {
+  ip := os.Getenv("IP")
+  port := os.Getenv("PORT")
+  if ip == "" {
+    ip = "localhost"
+  }
+  if port == "" {
+    port = "8000"
+  }
 	static := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static", static))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "index.html")
 	})
 
-	log.Panic(http.ListenAndServe("192.168.1.125:8008", nil))
+  log.Panic(http.ListenAndServe(ip + ":" + port, nil))
 }
